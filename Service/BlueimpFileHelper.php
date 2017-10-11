f2214d1e5575e1de97e8f6c0158a54fe6acfc035
marcin <marcin@parabolalab.com>
2017-10-11 20:52:51 +0200

    convert file entity to blueimp file array by service

---
+++ Service/BlueimpFileHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Parabol\FilesUploadBundle\Service;
+
+/**
+* BlueimpFile Service to convert File Entity to array for  bluimp jquery file upload  
+*/
+class BlueimpFileHelper
+{
+	private $router;
+	private $liipCacheManager;
+	
+	function __construct($router, $liipCacheManager)
+	{
+		$this->router = $router;
+		$this->liipCacheManager = $liipCacheManager;	
+	}
+
+	public function toArray(\Parabol\FilesUploadBundle\Entity\File $file, $allowDeletePattern = null, $thumbName = 'admin_thumb')
+	{
+		$result = [
+			"name" => basename($file->getPath()),
+			"url" => $file->getPath(), //$path ? $path : preg_replace('#^.*\/.\.\/[\w_]+#', '', $obj->getPathname());
+            "size" => $file->getSize(),
+            "sort" => $file->getSort(),
+            "id" => $file->getId(),
+            "width" => $file->getWidth(),
+            "height" => $file->getHeight(),
+            "thumbnailUrl" => !$file->isImage() ? $file->getPathForThumb() : $this->liipCacheManager->getBrowserPath($file->getPathForThumb(), $thumbName),
+            "deleteUrl" => !$allowDeletePattern || preg_match('#' . $allowDeletePattern . '#', $file->getPath()) ? $this->router->generate('parabol_uploader_delete', [ 'id' => $file->getId() ]) : null,
+            "deleteType" => "GET",
+            "cropper" => $file->isImage(),
+            "cropBoxData" => (array)$file->getCropBoxData(),
+
+		];
+
+		return $result;
+	}
+}
