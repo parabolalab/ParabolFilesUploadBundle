07fac52eada24f646d9d837fc60d2f1b7d6f6771
marcin <marcin@parabolalab.com>
2017-10-15 18:07:11 +0200

    installer

---
+++ Composer/AdminInstaller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Parabol\FilesUploadBundle\Composer;
+
+use Composer\Script\Event;
+
+/**
+ * @author Marcin Kalota <marcin@parabolalab.com>
+ */
+class AdminInstaller extends \Parabol\BaseBundle\Composer\BaseScriptHandler
+{
+
+}
