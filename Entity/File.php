9a7f17a496f721be8604f8213bda6d60d02802ed
Marcin Kalota <marcin.kalota@aliso.pl>
2021-11-22 17:20:33 +0100

    fix file extra data

--- Entity/File.php
+++ Entity/File.php
@@ -76,30 +76,12 @@ class File extends BaseEntity
      */
     private $isNew = true;
 
-     /**
-     * @ORM\Column(name="alt", type="string", length=255, nullable=true)
-     */
-    private $alt;
+    
 
      /**
      * @ORM\Column(name="name", type="string", length=255, nullable=true)
      */
     private $name;
-
-     /**
-     * @ORM\Column(name="headline", type="string", length=255, nullable=true)
-     */
-    private $headline;
-
-    /**
-     * @ORM\Column(name="subheadline", type="text", nullable=true)
-     */
-    private $subheadline;
-
-    /**
-     * @ORM\Column(name="url", type="string", length=500, nullable=true)
-     */
-    private $url;
 
     /**
      * @ORM\Column(name="color", type="string", length=150, nullable=true)
@@ -383,30 +365,6 @@ class File extends BaseEntity
     }
 
     /**
-     * Set alt
-     *
-     * @param string $alt
-     *
-     * @return File
-     */
-    public function setAlt($alt)
-    {
-        $this->alt = $alt;
-
-        return $this;
-    }
-
-    /**
-     * Get alt
-     *
-     * @return string
-     */
-    public function getAlt()
-    {
-        return $this->alt;
-    }
-
-    /**
      * Set name
      *
      * @param string $name
@@ -428,78 +386,6 @@ class File extends BaseEntity
     public function getName()
     {
         return $this->name;
-    }
-
-    /**
-     * Set headline
-     *
-     * @param string $headline
-     *
-     * @return File
-     */
-    public function setHeadline($headline)
-    {
-        $this->headline = $headline;
-
-        return $this;
-    }
-
-    /**
-     * Get headline
-     *
-     * @return string
-     */
-    public function getHeadline()
-    {
-        return $this->headline;
-    }
-
-    /**
-     * Set subheadline
-     *
-     * @param string $subheadline
-     *
-     * @return File
-     */
-    public function setSubheadline($subheadline)
-    {
-        $this->subheadline = $subheadline;
-
-        return $this;
-    }
-
-    /**
-     * Get subheadline
-     *
-     * @return string
-     */
-    public function getSubheadline()
-    {
-        return $this->subheadline;
-    }
-
-    /**
-     * Set url
-     *
-     * @param string $url
-     *
-     * @return File
-     */
-    public function setUrl($url)
-    {
-        $this->url = $url;
-
-        return $this;
-    }
-
-    /**
-     * Get url
-     *
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
     }
 
     /**
